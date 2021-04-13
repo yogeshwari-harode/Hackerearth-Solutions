00d41ddc55b73e6b5514fdfd8c6f34cf40196d53
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-04-13 12:48:45 +0530

    Create Print the Numbers.cpp

---
+++ Basics of Input-Output/Print the Numbers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long int i,n,arr[n];
+    cin>>n;
+
+    for(i=1; i<=n; i++)
+    {
+        cin>>arr[i];
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
