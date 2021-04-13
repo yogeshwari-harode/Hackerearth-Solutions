a92e6b2dcece708121199f9976c29fa7cc8dbdc8
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-04-13 12:50:44 +0530

    Create I am Easy.cpp

---
+++ Basics of Input-Output/I am Easy.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,t;
+    cin>>n;
+    for(int i=1; i<=10; i++)
+    {
+        t=n*i;
+        cout<<t<<endl;
+    }
+    return 0;
+}
